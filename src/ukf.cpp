503f211b6b79404c892bcebe2a12a54e6d72c39c
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-03-02 01:44:27 +0800

    add sigma points prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -54,6 +54,22 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  is_initialized_ = false;
+
+  std_a_ = 0.5;
+  std_yawdd_ = 0.04;
+
+  n_x_ = 5;
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_;
+
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  double denom = lambda_ + n_aug_;
+  weights_(0) = lambda_ / denom;
+  weights_.tail(2 * n_aug_) = VectorXd::Constant(2 * n_aug_, 1 / (2 * denom));
+
+  Xsig_pred_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+  P_.setIdentity();
 }
 
 UKF::~UKF() {}
@@ -69,6 +85,60 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_) {
+    cout << "UKF: " << endl;
+
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      auto rho = meas_package.raw_measurements_[0];
+      auto phi = meas_package.raw_measurements_[1];
+      auto px = cos(phi) * rho;
+      auto py = sin(phi) * rho;
+      x_ << px, py, 0, 0, 0;
+    } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0, 0, 0;
+    }
+
+    time_us_ = meas_package.timestamp_;
+    is_initialized_ = true;
+  }
+
+  float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = meas_package;
+
+  // step 1: generate sigma points with augmented x
+  VectorXd x_aug = VectorXd(n_aug_);
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+
+  x_aug.head(5) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(5, 5) = P_;
+  P_aug(5, 5) = std_a_ * std_a_;
+  P_aug(6, 6) = std_yawdd_ * std_yawdd_;
+
+  MatrixXd L = P_aug.llt().matrixL();
+  // use Xsig_pred_ to store sigma points before and after prediction
+  // note Xsig_pred_ is supposed to store n_x_ elements per column
+  // Xsig_aug_ is supposed to store n_aug_ elements per column
+  // to accommodate this I enlarged the size of Xsig_pred_
+  // the last two rows for Xsig_pred_ should be ignored when doing update
+  Xsig_pred_.col(0) = x_aug;
+  for (int i = 0; i < n_aug_; i++) {
+    Xsig_pred_.col(i + 1) = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
+    Xsig_pred_.col(i + n_aug_ + 1) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
+  }
+
+  // step 2: predict the ground_truth state at the next step
+  Prediction(dt);
+
+  // step 3: mapping the prediction to the measurement space happens here
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
+  } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    UpdateLidar(meas_package);
+  }
 }
 
 /**
@@ -83,6 +153,44 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  // step 1: predict sigma points for time step k + 1
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {
+    double p_x = Xsig_pred_(0, i);
+    double p_y = Xsig_pred_(1, i);
+    double v = Xsig_pred_(2, i);
+    double yaw = Xsig_pred_(3, i);
+    double yawd = Xsig_pred_(4, i);
+    double nu_a = Xsig_pred_(5, i);
+    double nu_yawdd = Xsig_pred_(6, i);
+
+    double px_pred, py_pred;
+
+    if (fabs(yawd) > 0.001) {
+      px_pred = p_x + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
+      py_pred = p_y + v / yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
+    } else {
+      px_pred = p_x + v * delta_t * cos(yaw);
+      py_pred = p_y + v * delta_t * sin(yaw);
+    }
+
+    double v_pred = v;
+    double yaw_pred = yaw + yawd * delta_t;
+    double yawd_pred = yawd;
+
+    px_pred += 0.5 * nu_a * delta_t * delta_t * cos(yaw);
+    py_pred += 0.5 * nu_a * delta_t * delta_t * sin(yaw);
+    v_pred += nu_a * delta_t;
+
+    yaw_pred += 0.5 * nu_yawdd * delta_t * delta_t;
+    yawd_pred += nu_yawdd * delta_t;
+
+    Xsig_pred_(0, i) = px_pred;
+    Xsig_pred_(1, i) = py_pred;
+    Xsig_pred_(2, i) = v_pred;
+    Xsig_pred_(3, i) = yaw_pred;
+    Xsig_pred_(4, i) = yawd_pred;
+  }
+  // step 2: compute mean and covariance matrix from predicted sigma points
 }
 
 /**
